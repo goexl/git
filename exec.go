612f08869de5f826c4cd4d9a0b8ba3c94d10375e
华寅 <storezhang@gmail.com>
2022-04-14 11:45:25 +0800

    feat(项目): 初始化

---
+++ exec.go
@@ -0,0 +1,9 @@
+package git
+
+import (
+	"github.com/goexl/gex"
+)
+
+func exec() (err error) {
+	return gex.Exec()
+}
---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/goexl/git
+
+go 1.17
+
+require (
+	github.com/goexl/gex v0.2.5
+)
